28f3e0547f3687af0372fbb6c05be292759f1fda
din <dina.vintr@gmail.com>
2025-08-17 21:54:46 +0300

    add mcp empty proxy and mcp inspector

---
+++ guard/main.py
@@ -0,0 +1,26 @@
+from fastmcp import FastMCP
+from fastmcp.server.proxy import ProxyClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+# mcp_proxy.py      
+config = {
+     "mcpServers": {
+        "default": {  # For single server configs, 'default' is commonly used
+            "url":  os.getenv("MCP_SERVER_URL"),
+            "transport": os.getenv("MCP_SERVER_TRANSPORT", "http"), 
+            "name": os.getenv("MCP_SERVER_NAME")
+        }
+    }
+}
+
+# Create a proxy with full MCP feature support
+proxy = FastMCP.as_proxy(
+    ProxyClient(config),
+    name=os.getenv("NAME", "mcp-guard")
+)
+
+# Run the proxy (e.g., via stdio for Claude Desktop)
+if __name__ == "__main__":
+    proxy.run(transport="http", host="0.0.0.0", port=os.getenv("PORT", 8080))  
--- host/app.cs
+++ host/app.cs
@@ -1,5 +1,50 @@
+#pragma warning disable ASPIREINTERACTION001
+#pragma warning disable ASPIREHOSTINGPYTHON001
+#pragma warning disable ASPIREPUBLISHERS001
+using static Aspire.Hosting.InputType;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
+var targetMcP = builder.AddParameter("mcp-server")
+    .WithDescription("The URL of the external service.")
+    .WithCustomInput(p => new()
+    {
+        InputType = Text,
+        Value = "https://aiam-mcps-everything.cfapps.eu12.hana.ondemand.com/mcp",
+        Options =
+        [
+            new KeyValuePair<string, string>("everything",
+                "https://aiam-mcps-everything.cfapps.eu12.hana.ondemand.com/mcp"),
+            new KeyValuePair<string, string>("github", "https://api.githubcopilot.com/mcp/"),
+        ],
+        Label = p.Name,
+        Placeholder = $"Select or type mcp server to gaurd {p.Name}",
+        Description = p.Description
+    }).WithDescription("Target McP server URL");
 
+
+
+var pythonProxy = builder
+    .AddPythonApp("guard", "../guard", "main.py")
+    .WaitFor(targetMcP)
+    .WithHttpEndpoint(port: 7000, env: "PORT", name: "http")
+    .WithEnvironment("MCP_SERVER_URL", targetMcP)
+  //  .WithOtlpExporter()
+    .WithExternalHttpEndpoints();
+
+builder
+    .AddMcpInspector("inspector")
+    .WithMcpServer(pythonProxy, isDefault: true)
+    .WaitFor(pythonProxy)
+    .WithEnvironment("MCP_SERVER_URL", pythonProxy.GetEndpoint("http"))
+    .WithEnvironment("DEFAULT_MCP_SERVER", pythonProxy.Resource.Name)
+    .WithUrlForEndpoint(McpInspectorResource.ClientEndpointName, annotation =>
+    {
+        annotation.DisplayText = "Client";
+        annotation.DisplayOrder = 2;
+    })
+
+   // .WithOtlpExporter()
+    ;
 
 builder.Build().Run();
